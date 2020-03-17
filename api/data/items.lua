448e97a844d221eb71f62d7ba6f9befa1c4b88e8
Vincent Lelong <vincent.lelong@epitech.eu>
2020-03-17 18:39:31 +0100

    Add a table to get plethora modules names

--- api/data/items.lua
+++ api/data/items.lua
@@ -1,4 +1,16 @@
 tools = {
+	plethoraModule = "plethora:module",
 	scanner = "plethora:scanner",
 	pickaxe = "minecraft:diamond_pickaxe"
+}
+
+plethoraModules = {
+	[0] = "plethora:introspection",
+	"plethora:laser",
+	"plethora:scanner",
+	"plethora:sensor",
+	"plethora:kinetic",
+	"plethora:chat",
+	"plethora:glasses",
+	"plethora:chat_creative",
 }
