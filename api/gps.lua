5dd49907616f24a6f542c0de147a8ca684c77043
Vincent Lelong <vincent.lelong@epitech.eu>
2020-03-19 20:19:38 +0100

    Feature (gps): Store map in a file to remember the path taken

--- api/gps.lua
+++ api/gps.lua
@@ -1,7 +1,8 @@
 os.loadAPI("api/inventory.lua")
 
 local w1 = 1
-local w2 = 1
+local w2 = 10
+local pathUpdateFrequency = 8
 
 -- Node struct
 local Node = {}
@@ -33,63 +34,69 @@ local function ManhattanDistance(v1, v2)
 	return math.abs(v1.x - v2.x) + math.abs(v1.y - v2.y) + math.abs(v1.z - v2.z)
 end
 
-local function ShortestPath(goal, scanner)
-	local closedList = {}
-	local openedList = { Node(vector.new(0, 0, 0), 0, 0, 0) }
+local function GetMapKey(vector)
+	return vector.x .. "," .. vector.y .. "," .. vector.z
+end
 
-	while next(openedList) ~= nil do
+local function GetDirection(displacement)
+	if displacement.x == 1 then return east end
+	if displacement.x == -1 then return west end
+	if displacement.y == 1 then return up end
+	if displacement.y == -1 then return down end
+	if displacement.z == 1 then return south end
+	if displacement.z == -1 then return north end
+	return nil
+end
+
+local function ShortestPath(goal, map)
+	local currentPosition = Locate()
+	local closedList = {}
+	local openedList = { Node(currentPosition, 0, 0, 0) }
+	
+	while #openedList > 0 do
 		os.queueEvent("fakeEvent");
 		os.pullEvent();
 		local currentNode = table.remove(openedList, 1)
-		local successors = {}
-
-		if currentNode.position.z + 1 < 10 and scanner:IsEmptyBlock(currentNode.position.x, currentNode.position.y, currentNode.position.z + 1) then
-			table.insert(successors, Node(vector.new(currentNode.position.x, currentNode.position.y, currentNode.position.z + 1), 0, 0, 0))
-		end
-		if currentNode.position.z - 1 > -10 and scanner:IsEmptyBlock(currentNode.position.x, currentNode.position.y, currentNode.position.z - 1) then
-			table.insert(successors, Node(vector.new(currentNode.position.x, currentNode.position.y, currentNode.position.z - 1), 0, 0, 0))
-		end
-		if currentNode.position.x + 1 < 10 and scanner:IsEmptyBlock(currentNode.position.x + 1, currentNode.position.y, currentNode.position.z) then
-			table.insert(successors, Node(vector.new(currentNode.position.x + 1, currentNode.position.y, currentNode.position.z), 0, 0, 0))
-		end
-		if currentNode.position.x - 1 > -10 and scanner:IsEmptyBlock(currentNode.position.x - 1, currentNode.position.y, currentNode.position.z) then
-			table.insert(successors, Node(vector.new(currentNode.position.x - 1, currentNode.position.y, currentNode.position.z), 0, 0, 0))
-		end
-		if currentNode.position.y + 1 < 10 and scanner:IsEmptyBlock(currentNode.position.x, currentNode.position.y + 1, currentNode.position.z) then
-			table.insert(successors, Node(vector.new(currentNode.position.x, currentNode.position.y + 1, currentNode.position.z), 0, 0, 0))
-		end
-		if currentNode.position.y - 1 > -10 and scanner:IsEmptyBlock(currentNode.position.x, currentNode.position.y - 1, currentNode.position.z) then
-			table.insert(successors, Node(vector.new(currentNode.position.x, currentNode.position.y - 1, currentNode.position.z), 0, 0, 0))
-		end
-
+		local successors = {
+			Node(vector.new(currentNode.position.x, currentNode.position.y - 1, currentNode.position.z), 0, 0, 0),
+			Node(vector.new(currentNode.position.x, currentNode.position.y, currentNode.position.z + 1), 0, 0, 0),
+			Node(vector.new(currentNode.position.x, currentNode.position.y, currentNode.position.z - 1), 0, 0, 0),
+			Node(vector.new(currentNode.position.x + 1, currentNode.position.y, currentNode.position.z), 0, 0, 0),
+			Node(vector.new(currentNode.position.x - 1, currentNode.position.y, currentNode.position.z), 0, 0, 0),
+			Node(vector.new(currentNode.position.x, currentNode.position.y + 1, currentNode.position.z), 0, 0, 0),
+		}
+		
 		for i = 1, #successors do
 			local currentSuccessor = successors[i]
 			currentSuccessor.parent = currentNode
-
+			
 			if currentSuccessor.position:tostring() == goal:tostring() then
-				while currentSuccessor.parent.position:tostring() ~= vector.new(0, 0, 0):tostring() do
+				local movements = {}
+				while currentSuccessor.parent do
+					local displacement = currentSuccessor.position - currentSuccessor.parent.position
+					table.insert(movements, 1, GetDirection(displacement))
 					currentSuccessor = currentSuccessor.parent
 				end
-
-				if currentSuccessor.position.x == 1 then return east end
-				if currentSuccessor.position.x == -1 then return west end
-				if currentSuccessor.position.y == 1 then return up end
-				if currentSuccessor.position.y == -1 then return down end
-				if currentSuccessor.position.z == 1 then return south end
-				if currentSuccessor.position.z == -1 then return north end
-
-				return nil
+				return movements
 			end
-
+			
 			currentSuccessor.distance = currentNode.distance + 1
 			currentSuccessor.heuristic = ManhattanDistance(currentSuccessor.position, goal)
 			currentSuccessor.cost = w1 * currentSuccessor.distance + w2 * currentSuccessor.heuristic
-
-			local shouldSkip = false
-			for j = 1, #openedList do
-				if openedList[j].position:tostring() == currentSuccessor.position:tostring() and openedList[j].cost <= currentSuccessor.cost then
-					shouldSkip = true
-					break
+			
+			local block = map[GetMapKey(currentSuccessor.position)]
+			if block == nil then
+				currentSuccessor.distance = currentSuccessor.distance + 1
+				block = false
+			end
+	
+			local shouldSkip = block
+			if not shouldSkip then
+				for j = 1, #openedList do
+					if openedList[j].position:tostring() == currentSuccessor.position:tostring() and openedList[j].cost <= currentSuccessor.cost then
+						shouldSkip = true
+						break
+					end
 				end
 			end
 			if not shouldSkip then
@@ -100,8 +107,7 @@ local function ShortestPath(goal, scanner)
 					end
 				end
 			end
-
-			if shouldSkip == false then
+			if not shouldSkip then
 				local k = 1
 				while k <= #openedList and openedList[k].cost < currentSuccessor.cost do
 					k = k + 1
@@ -114,24 +120,17 @@ local function ShortestPath(goal, scanner)
 	end
 end
 
-local function GetGoal(arrival, scanner)
+local function SaveScanToMap(map, scanner)
 	local worldPosition = Locate()
-	local closestPosition = vector.new(0, 0, 0)
 
-	local closestDistance = ManhattanDistance(arrival, worldPosition)
-	local minDistance = scanner:GetRadius() * -1 - 1
-	local maxDistance = scanner:GetRadius() + 1
+	local minDistance = scanner:GetRadius() * -1
+	local maxDistance = scanner:GetRadius()
 
 	for x = minDistance, maxDistance do
 		for y = minDistance, maxDistance do
 			for z = minDistance, maxDistance do
-				if scanner:IsEmptyBlock(x, y, z) then
-					local position = worldPosition + vector.new(x, y, z)
-					local distance = ManhattanDistance(arrival, position)
-					if distance < closestDistance then
-						closestPosition = vector.new(x, y, z)
-						closestDistance = distance
-					end
+				if not (x == 0 and y == 0 and z == 0) then
+					map[GetMapKey(worldPosition + vector.new(x, y, z))] = not scanner:IsEmptyBlock(x, y, z)
 				end
 			end
 		end
@@ -140,23 +139,56 @@ local function GetGoal(arrival, scanner)
 	return closestPosition
 end
 
+local function LoadMap()
+	local map = {}
+	local mapFS = fs.open("api/data/map", "r")
+
+	if mapFS then
+		local line = mapFS.readLine()
+		while line do
+			map[line] = true
+			line = mapFS.readLine()
+		end
+		mapFS.close()
+	end
+	return map
+end
+
+local function SaveMap(map)
+	local mapFS = fs.open("api/data/map", "w")
+
+	for key,value in pairs(map) do
+		if value then
+			mapFS.writeLine(key)
+		end
+	end
+
+	mapFS.close()
+end
 
 -- PUBLIC
 function MoveTo(arrival)
 	local scanner = inventory.EquipScanner()
 	local position = Locate()
+	local map = LoadMap()
+	local moveBeforeUpdatePath = 0
+	local directions
 
 	while position:tostring() ~= arrival:tostring() do
 		scanner:Scan()
-
-		local goal = GetGoal(arrival, scanner)
-		local direction = ShortestPath(goal, scanner)
-
-		if direction == nil then
+		SaveScanToMap(map, scanner)
+		
+		if moveBeforeUpdatePath == 0 or #directions == 0 then
+			directions = ShortestPath(arrival, map)
+			moveBeforeUpdatePath = pathUpdateFrequency
+		end
+		
+		if directions == nil then
 			print("ERROR: no path found")
 			return
 		end
-
+		
+		local direction = table.remove(directions, 1)
 		if direction == up then
 			turtle.up();
 		elseif direction == down then
@@ -167,9 +199,16 @@ function MoveTo(arrival)
 			end
 			turtle.forward()
 		end
-
+		
 		position = Locate()
+		moveBeforeUpdatePath = moveBeforeUpdatePath - 1
+
+		while #map > 900000 do
+			table.remove(map, 1)
+		end
 	end
+	
+	SaveMap(map)
 end
 
 function FaceDirection(direction)
